bbfc11f195c474f4f0d017ca5b13f0a61a031976
Gregor <gregor.karpljuk@gmail.com>
2019-05-05 18:52:08 +0200

    API: WIP on time etc.

--- express/controllers/UserTaskController.js
+++ express/controllers/UserTaskController.js
@@ -6,8 +6,7 @@ module.exports = {
             try {
                 let UserTasks = await UserTaskModel.find({}).exec();
                 return res.json(UserTasks);
-            }
-            catch (err) {
+            } catch (err) {
                 return res.status(500).json({
                     message: 'Error when getting UserTask.',
                     error: err
@@ -24,10 +23,9 @@ module.exports = {
                 query = req.headers.query;
 
             try {
-                let UserTasks = await UserTaskModel.find({ storyId: req.params.storyId }).exec();
+                let UserTasks = await UserTaskModel.find({storyId: req.params.storyId}).exec();
                 return res.json(UserTasks);
-            }
-            catch (err) {
+            } catch (err) {
                 return res.status(500).json({
                     message: 'Error when getting UserTask.',
                     error: err
@@ -39,7 +37,7 @@ module.exports = {
     findOne(req, res) {
         const id = req.params.id;
 
-        UserTaskModel.findOne({ _id: id }, function (err, UserStory) {
+        UserTaskModel.findOne({_id: id}, function (err, UserStory) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting UserTask.',
@@ -62,7 +60,7 @@ module.exports = {
         let description = req.body.description;
         let sId = req.body.storyId;
 
-        UserTaskModel.findOne({ description: description, storyId: sId }, function (err, UserTask) {
+        UserTaskModel.findOne({description: description, storyId: sId}, function (err, UserTask) {
             if (UserTask) {
                 return res.status(404).json({
                     message: 'Naloga s tem imenom že obstaja za to zgodbo'
@@ -87,7 +85,7 @@ module.exports = {
     update(req, res) {
         const id = req.params.id;
 
-        UserTaskModel.findOneAndUpdate({ _id: id }, { $set: req.body }, { new: true }, function (err, UserTask) {
+        UserTaskModel.findOneAndUpdate({_id: id}, {$set: req.body}, {new: true}, function (err, UserTask) {
             if (err) {
                 console.log("ERR: ", err);
                 return res.status(500).json({
@@ -110,10 +108,10 @@ module.exports = {
         const taskId = req.query.taskId;
         const task = req.body;
 
-        UserTaskModel.findOneAndUpdate({ _id: taskId, assignee: { $eq: null } },
-            { $set: { assignee: task.assignee, accepted: true } }, { new: true }, function (err, UserTask) {
+        UserTaskModel.findOneAndUpdate({_id: taskId, assignee: {$eq: null}},
+            {$set: {assignee: task.assignee, accepted: true}}, {new: true}, function (err, UserTask) {
                 console.log(taskId, UserTask);
-            
+
                 if (err) {
                     console.log("ERR: ", err);
                     return res.status(500).json({
@@ -134,10 +132,10 @@ module.exports = {
     unassign(req, res) {
         const taskId = req.query.taskId;
 
-        UserTaskModel.findOneAndUpdate({ _id: taskId, assignee: { $ne: null } },
-            { $set: { assignee: null, accepted: false } }, { new: true }, function (err, UserTask) {
+        UserTaskModel.findOneAndUpdate({_id: taskId, assignee: {$ne: null}},
+            {$set: {assignee: null, accepted: false}}, {new: true}, function (err, UserTask) {
                 console.log(taskId, UserTask);
-            
+
                 if (err) {
                     console.log("ERR: ", err);
                     return res.status(500).json({
@@ -157,11 +155,10 @@ module.exports = {
 
     changeActiveStatus(req, res) {
         const taskId = req.params.taskId;
-        const data = req.body;
 
         console.log(req.body);
 
-        UserTaskModel.findOne({ _id: taskId }, function (err, UserTask) {
+        UserTaskModel.findOne({_id: taskId}, function (err, UserTask) {
             if (err) {
                 return res.status(500).json({
                     message: 'An error occured while fetching task.',
@@ -175,21 +172,81 @@ module.exports = {
                 });
             }
 
-            UserTask.active = data.isActive;
-
-            if (!data.isActive) {
+            if (UserTask.active) {
                 UserTask.activeHours += 1;
+
+                let todaysDate = new Date();
+                console.log("todaysDate", todaysDate);
+
+                for (let i = 0; i < UserTask.activities.length; i++) {
+                    let dbDate = UserTask.activities[i].date;
+
+                    if (dbDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+                        console.log("i, todaysActivityFound = true;", i);
+
+
+                        let timeDifference = new Date() - UserTask.activities[i].date;
+                        let timeDifferenceHours = Math.ceil((timeDifference / 1000) / 60 / 60);
+
+                        /*
+                        timeToAdd.setHours(timeToAdd.getHours() + Math.round(timeToAdd.getMinutes()/60));
+                        timeToAdd.setMinutes(0);
+                         */
+                        console.log("UserTask.activities[i].date, new Date()", UserTask.activities[i].date, new Date());
+                        console.log("timeDifferenceHours", timeDifferenceHours);
+
+                        UserTask.activities[i].activeHours += timeDifferenceHours;
+
+                        console.log("UserTask.activities[i].date___START", UserTask.activities[i].date);
+                        UserTask.activities[i].date = null;
+                        UserTask.activities[i].date = new Date();
+                        console.log("UserTask.activities[i].date___END", UserTask.activities[i].date);
+                    }
+                }
             }
+
+            /*
+
+            if (UserTask.active) {
+                //Task is active, user requested a change
+                //Change status to inactive
+                UserTask.active = false;
+            } else {
+                //Task is inactive, user requested a change
+                //Change status to active
+                UserTask.active = true;
+
+                let todaysDate = new Date();
+                console.log("todaysDate", todaysDate);
+                let todaysActivityFound = false;
+
+                for (let i = 0; i < UserTask.activities.length; i++) {
+                    let dbDate = UserTask.activities[i].date;
+                    console.log("UserTask.activities[i].date", UserTask.activities[i].date);
+
+                    if (dbDate.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+                        todaysActivityFound = true;
+                        console.log("i, todaysActivityFound = true;", i);
+                    }
+                }
+
+                if (!todaysActivityFound) {
+                    UserTask.activities.push({"user": UserTask.assignee, "date": new Date(), "activeHours": 0})
+                }
+
+            }
+
+             */
 
             UserTask.save(function (err, UserTask) {
                 if (err) {
                     return res.status(500).json({
-                        message: 'Error when updating UserTask.',
+                        message: 'Error while updating UserTask.',
                         error: err
                     });
                 }
 
-                console.log(UserTask);
+                //console.log(UserTask);
 
                 return res.json(UserTask);
             });
